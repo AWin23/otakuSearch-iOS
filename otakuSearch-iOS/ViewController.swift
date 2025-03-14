0c1f5a5449beecb3f908e3c30638f3879a54364e
Andrew Nguyen <nguyendrew20@gmail.com>
2025-03-13 22:31:08 -0700

    Initial Commit

---
+++ otakuSearch-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  otakuSearch-iOS
+//
+//  Created by Andrew Nguyen on 3/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ otakuSearch-iOSTests/otakuSearch_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  otakuSearch_iOSTests.swift
+//  otakuSearch-iOSTests
+//
+//  Created by Andrew Nguyen on 3/13/25.
+//
+
+import Testing
+@testable import otakuSearch_iOS
+
+struct otakuSearch_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
